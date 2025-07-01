7f8cde4ab0fd50c61cce013d0bb228355c9d16e1
naxosit <128820710+naxosit@users.noreply.github.com>
2025-06-30 22:59:51 -0400

    Se modifica la tabla Cine cambiando Ubicacion -> idCiudad (junto con todas las restricciones correspondientes: Tipo de dato, fk, etc.)

--- BDD/Etapa 3/Ciudad y Region.sql
+++ BDD/Etapa 3/Ciudad y Region.sql
@@ -14,4 +14,20 @@ CREATE TABLE Ciudad (
 	FOREIGN KEY (idRegion) REFERENCES Region(idRegion)
 );
 
+-- Modificar tabla Cine, primero con el nombre del atributo
+ALTER TABLE Cine
+RENAME COLUMN Ubicacion TO idCiudad
+
+-- Los atributos previamente insertados los pasamos a NULL para que nos deje continuar
+-- con el siguiente paso
+UPDATE Cine 
+SET idCiudad = NULL
+
+-- Ahora cambiamos el tipo de atributo
+ALTER TABLE Cine
+ALTER COLUMN idCiudad TYPE BIGINT USING (idCiudad::BIGINT); 
+
+-- Finalmente, añadimos la restricción de la clave foránea
+ALTER TABLE Cine
+ADD CONSTRAINT fk_idCiudad FOREIGN KEY (idCiudad) REFERENCES Ciudad (idCiudad);
 
