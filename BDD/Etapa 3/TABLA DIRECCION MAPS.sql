c662d0b059900d3d6de9a54bae5fbef73b24e32f
naxosit <128820710+naxosit@users.noreply.github.com>
2025-07-03 13:28:25 -0400

    Modificaciones varias a tabla de direcciones maps.

--- BDD/Etapa 3/TABLA DIRECCION MAPS.sql
+++ BDD/Etapa 3/TABLA DIRECCION MAPS.sql
@@ -1,12 +1,15 @@
-CREATE TABLE DireccionMaps (
-	idCine BIGINT,
-	idCiudad BIGINT,
-	URL TEXT,
-	PRIMARY KEY (idCine),
-	FOREIGN KEY (idCiudad) REFERENCES Ciudad(idCiudad),
-	FOREIGN KEY (idCine) REFERENCES Cine(idCine)
+-- Creada con identidad correcta
+CREATE TABLE direccionmaps (
+    iddireccion BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    idcine      BIGINT,
+    idciudad    BIGINT,
+    url         TEXT,
+    FOREIGN KEY (idciudad) REFERENCES ciudad(idciudad),
+    FOREIGN KEY (idcine) REFERENCES cine(idcine)
 );
-
 -- Inserción de cine Meyer y cine Chuyaca
 INSERT INTO DireccionMaps (idCine, idCiudad, URL) VALUES (2, 10301, 'https://www.google.cl/maps/place/Universidad+de+Los+Lagos/@-40.5851249,-73.0930591,15.5z/data=!4m6!3m5!1s0x961636074299eb0b:0x9666c6a322034b6b!8m2!3d-40.586833!4d-73.0894195!16zL20vMDRwMDNw?hl=es&entry=ttu&g_ep=EgoyMDI1MDYyOS4wIKXMDSoASAFQAw%3D%3D');
 INSERT INTO DireccionMaps (idCine, idCiudad, URL) VALUES (1, 10301, 'https://www.google.cl/maps/place/Universidad+De+Los+Lagos+(MEYER)/@-40.6038904,-73.1121674,14z/data=!4m6!3m5!1s0x961635ef5d0df291:0x80a0581c66373a2d!8m2!3d-40.5977313!4d-73.1040146!16s%2Fg%2F11b7q1sqlr?hl=es&entry=ttu&g_ep=EgoyMDI1MDYyOS4wIKXMDSoASAFQAw%3D%3D');
+
+DROP TABLE DireccionMaps;
+
